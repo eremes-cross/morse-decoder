6275b676e8d23045d4f6b35afc865249c3d8dc42
eremes-cross <sergei.cross@yandex.by>
2022-07-29 19:29:57 +0300

    morse

--- src/index.js
+++ src/index.js
@@ -37,8 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) { 
+    
+    function wordDecoder(el) {
+      
+        let unCut;
+        let letter;
+        let letters = [];     
+      
+          for (let i = 0; i < el.length; i += 10) { 
+            unCut = el.slice(i, i + 10);
+            letter = unCut.slice(unCut.indexOf(1)).replace(/10/g, `.`).replace(/11/g, `-`);
+            letters.push(letter);
+          } 
+        
+          for (let i = 0; i < letters.length; i++) {
+            if (letters[i] in MORSE_TABLE) {letters[i] = MORSE_TABLE[letters[i]]} 
+          } 
+          
+        el = letters.join('');
+        return el;    
+        }
+
+    
+    
+    return expr.split(`**********`).map(item => wordDecoder(item)).join(' ');
+
 }
 
 module.exports = {
